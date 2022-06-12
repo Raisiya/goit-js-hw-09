c86baa66270e6262dffcc97206266d77d7e85239
Raya Shcherban <90385588+Raisiya@users.noreply.github.com>
2022-06-13 00:03:45 +0200

    1-task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+const PROMPT_DELAY = 1000;
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', srartChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function srartChangeColor() {
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
+    intervalId = setInterval(changeColor, 1000);
+};
+
+function stopChangeColor() {
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', true);
+    clearInterval(intervalId);
+};
+
+function changeColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
